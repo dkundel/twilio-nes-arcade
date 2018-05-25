d4b3c2c63ce90106b1e3373b21d03ee4f0d112a6
Dominik Kundel <dominik.kundel@gmail.com>
2018-05-25 20:32:21 +0200

    :sparkles: Support emoji based button instructions

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
 import { NES, Controller } from 'jsnes';
+import cloneDeep from 'lodash.clonedeep';
+import { unemojify } from 'node-emoji';
 
 import Screen from './Screen';
 
 const GAME_URL =
   'https://d3cto2l652k3y0.cloudfront.net/Super%20Mario%20Bros.%20(JU)%20(PRG0)%20[!].nes';
 const DEFAULT_SPEED = 1;
+const DURATION_OF_BUTTON_IN_FRAMES = 20;
+const EMOJI_BUTTON_MAP = {
+  ':arrow_up:': Controller.BUTTON_UP,
+  ':arrow_down:': Controller.BUTTON_DOWN,
+  ':arrow_left:': Controller.BUTTON_LEFT,
+  ':arrow_right:': Controller.BUTTON_RIGHT,
+  ':rocket:': Controller.BUTTON_A,
+  ':fist:': Controller.BUTTON_B,
+  ':raised_hand:': Controller.BUTTON_START,
+  ':point_right:': Controller.BUTTON_SELECT
+};
+
+function convertEmojisToButtons(text) {
+  const emojiString = unemojify(text);
+  const button = EMOJI_BUTTON_MAP[emojiString];
+  console.log(text, emojiString, button);
+  if (typeof button !== 'undefined') {
+    return button;
+  }
+  return -1;
+}
+
+function convertToButtonCommandsPerFrame(buttons) {
+  const instructions = [];
+  for (const button of buttons) {
+    instructions.push({ button, mode: 'press' });
+    for (let i = 0; i < DURATION_OF_BUTTON_IN_FRAMES - 2; i++) {
+      instructions.push({ mode: 'none' });
+    }
+    instructions.push({ button, mode: 'release' });
+  }
+  return instructions;
+}
 
 class Game extends Component {
   constructor(props) {
@@ -14,7 +49,14 @@ class Game extends Component {
       speed: DEFAULT_SPEED
     };
     this.gameLoop = this.gameLoop.bind(this);
+    this.buttonInstructions = [];
   }
+
+  shouldComponentUpdate() {
+    // For now let's just never re-render;
+    return false;
+  }
+
   async loadRom() {
     // I should change this to fetch. This is taken from the jsnes-web project
     return new Promise((resolve, reject) => {
@@ -39,50 +81,53 @@ class Game extends Component {
 
   async componentDidMount() {
     const romData = await this.loadRom();
+    const memory = await (await fetch('/gameData.json')).json();
+    memory.romData = romData;
+    this.gameMemoryData = memory;
     this.nes = new NES({
       onFrame: this.screen.setBuffer,
       onStatusUpdate: console.log
     });
-    const memory = await (await fetch('/gameData.json')).json();
-    memory.romData = romData;
-    this.gameMemoryData = memory;
+  }
+
+  reset() {
     this.initGame();
-    this.buttons = [
-      Controller.BUTTON_RIGHT,
-      Controller.BUTTON_RIGHT,
-      Controller.BUTTON_RIGHT,
-      Controller.BUTTON_RIGHT,
-      Controller.BUTTON_RIGHT,
-      Controller.BUTTON_RIGHT,
-      Controller.BUTTON_RIGHT,
-      Controller.BUTTON_RIGHT,
-      Controller.BUTTON_RIGHT,
-      Controller.BUTTON_RIGHT,
-      Controller.BUTTON_RIGHT
-      // Controller.BUTTON_LEFT,
-      // Controller.BUTTON_LEFT,
-      // Controller.BUTTON_LEFT
-    ];
-    window.requestAnimationFrame(this.gameLoop);
+    this.renderLoop = window.requestAnimationFrame(this.gameLoop);
+  }
+
+  setButtons(buttons) {
+    this.buttons = [...buttons]
+      .map(convertEmojisToButtons)
+      .filter(x => x !== -1);
+    console.log(this.buttons);
+    this.buttonInstructions = convertToButtonCommandsPerFrame(this.buttons);
   }
 
   gameLoop() {
-    if (this.buttons[0]) {
-      this.simulateButtonPress(this.buttons[0]);
-      this.buttons = this.buttons.slice(1);
-    }
-
     // Skip frames depending on the speed (for speed runs)
     for (let x = 0; x < this.state.speed; x++) {
+      if (this.buttonInstructions.length > 0) {
+        const { button, mode } = this.buttonInstructions[0];
+        if (mode === 'release') {
+          this.nes.buttonUp(1, button);
+        } else if (mode === 'press') {
+          this.nes.buttonDown(1, button);
+        }
+        this.buttonInstructions = this.buttonInstructions.slice(1);
+      }
       this.nes.frame();
     }
 
     this.screen.writeBuffer();
-    window.requestAnimationFrame(this.gameLoop);
+    this.renderLoop = window.requestAnimationFrame(this.gameLoop);
   }
 
   initGame() {
-    this.nes.fromJSON(this.gameMemoryData);
+    if (this.renderLoop) {
+      window.cancelAnimationFrame(this.renderLoop);
+      this.renderLoop = undefined;
+    }
+    this.nes.fromJSON(cloneDeep(this.gameMemoryData));
     // this.skipGameScreen();
   }
 
--- src/pages/TryGame.js
+++ src/pages/TryGame.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
+import { Controller } from 'jsnes';
 import Game from '../components/Game';
 
 class TryGame extends Component {
+  constructor(props) {
+    super(props);
+    this.resetGame = this.resetGame.bind(this);
+    this.setButtons = this.setButtons.bind(this);
+  }
+
+  resetGame() {
+    this.game.reset();
+  }
+
+  setButtons() {
+    const buttons = '➡️➡️🚀⬅️⬅️';
+    this.game.setButtons(buttons);
+    this.game.reset();
+  }
+
   render() {
     return (
       <div>
         <h1>Try your fate</h1>
-        <Game />
+        <Game ref={game => (this.game = game)} />
+        <div>
+          <input type="text" name="buttonSeries" />
+          <button onClick={this.setButtons}>Set Buttons</button>
+          <button onClick={this.resetGame}>Reset Game</button>
+        </div>
       </div>
     );
   }
